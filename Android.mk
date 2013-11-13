46e6c2c6b07dc7c05030b831682a185a4dd7691c
git-rchow.invensense.com <rchow@invensense.com>
2013-11-13 13:51:56 -0800

    MotionApps 5.2.0.K Release Candidate 4 - This is the official release compatiable to Android KitKat - Supports "SENSORS_DEVICE_API_VERSION_1_1" API which includes   Step Detector, Step Count, Batch, Flush and Geomagnetic Sensor. - Tested on Nexus 5 / Hammerhead
    Change-Id: Ida22bb8ad83b57c1d35353c746a79897bf787f25
    Signed-off-by: git-rchow.invensense.com <rchow@invensense.com>

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 # Can't have both 65xx and 60xx sensors.
 ifneq ($(filter hammerhead, $(TARGET_DEVICE)),)
 # hammerhead expects 65xx sensors.
-include $(call all-named-subdir-makefiles,65xx)
+include $(call all-named-subdir-makefiles,6515)
 else
 # manta expects 60xx sensors.
 include $(call all-named-subdir-makefiles,60xx)
